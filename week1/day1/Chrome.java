3b66df8251f6594da302918f69b618cafd2d7881
R-sivaraman <135146412+R-sivaraman@users.noreply.github.com>
2024-12-07 22:50:45 +0530

    Add files via upload
    Week 1 Assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer ="Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'c';
+		
+	System.out.println("Chrome version is "+version);
+	System.out.println("Developed by "+developer);
+	System.out.println("Browser doesn't support beta is "+isBeta);
+	System.out.println("Browser released year is "+releaseYear);
+	System.out.println("Shortcut key is "+shortcutKey);
+			
+	}
+
+}
